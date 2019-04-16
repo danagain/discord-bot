a2f8ad6ae603020b80faaf9a80c82ed98cba1c4b
Daniel Huffer <danielhuffer@live.com>
2019-04-16 15:53:01 +1000

    latest

--- functions/dispatcher.js
+++ functions/dispatcher.js
@@ -8,33 +8,12 @@ dispatcher = {
         if(this.dispatcher == null) {
             this.dispatcher = connection.playStream(stream, streamOptions);
             this.dispatcher.on("end", end => {
-                // console.log("end inside startYTDispatcher");
-                // console.log("left channel");
-                this.dispatcher = null;
-                //message.member.voiceChannel.leave();
-
-                if(pl.pl.length > 0){
-                    message.member.voiceChannel.join()
-                    .then(connection => { // Connection is an instance of VoiceConnection
-                      const streamOptions = { seek: 0, volume: 1 };
-                          console.log("joined channel");
-                          const stream = ytdl("https://www.youtube.com/watch?v=B34DmsMxUlA", { filter : 'audioonly' });
-                          try{
-                             this.startYTDispatcher(message, connection, stream, streamOptions);
-                             this.dispatcher = connection.playStream(stream, streamOptions);
-                          }catch(error) {
-                            console.log(error);
-                          }
-                    })
-                    .catch(console.log('error'));
-                }else{
-                    console.log("end inside startYTDispatcher");
-                    console.log("left channel");
-                    this.dispatcher = null;
-                    message.member.voiceChannel.leave();
-                }
+            console.log("end inside startYTDispatcher");
+            console.log("left channel");
+            this.dispatcher = null;
+            message.member.voiceChannel.leave();
             });
-        }else {
+        } else {
             pl.addRequest(url);
             message.reply("There is already something playing!");
         }
